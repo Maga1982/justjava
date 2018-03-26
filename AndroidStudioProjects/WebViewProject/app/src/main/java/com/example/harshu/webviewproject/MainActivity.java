52fa3e4d932e03059e76e32d7aa74dc9d02d267c
Maga1982 <36678832+Maga1982@users.noreply.github.com>
2018-03-25 22:59:23 -0400

    iiii

---
+++ AndroidStudioProjects/WebViewProject/app/src/main/java/com/example/harshu/webviewproject/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.harshu.webviewproject;
+
+import android.content.Intent;
+import android.speech.tts.TextToSpeech;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+    public void but(View view){
+        Intent in = new Intent(this,WebViewActivity.class);
+        startActivity(in);
+    }
+
+}
---
+++ AndroidStudioProjects/WebViewProject/app/src/main/java/com/example/harshu/webviewproject/WebViewActivity.java
@@ -0,0 +1,91 @@
+package com.example.harshu.webviewproject;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.tts.TextToSpeech;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.ValueCallback;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.TextView;
+
+import java.util.Locale;
+
+public class WebViewActivity extends AppCompatActivity {
+
+    TextToSpeech textspeech_obj;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.weblayout1);
+
+        Intent intent = getIntent();
+
+        WebView webView = (WebView) findViewById(R.id.webid);
+
+        TextView contentView = (TextView) findViewById(R.id.textid);
+
+        textspeech_obj = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if (status != TextToSpeech.ERROR) {
+                    textspeech_obj.setLanguage(Locale.US);
+                }
+            }
+        });
+
+
+
+        /* An instance of this class will be registered as a JavaScript interface */
+        class MyJavaScriptInterface {
+            private TextView contentView;
+
+            public MyJavaScriptInterface(TextView aContentView) {
+                contentView = aContentView;
+            }
+
+            @SuppressWarnings("unused")
+
+            @JavascriptInterface
+            public void processContent(String aContent) {
+                final String content = aContent;
+                contentView.post(new Runnable() {
+                    public void run() {
+                        // contentView.setText(content);
+
+                        textspeech_obj.speak(content, TextToSpeech.QUEUE_FLUSH, null);
+                    }
+                });
+            }
+        }
+
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.addJavascriptInterface(new MyJavaScriptInterface(contentView), "INTERFACE");
+        webView.setWebViewClient(new WebViewClient() {
+            @Override
+            public void onPageFinished(WebView view, String url) {
+                view.loadUrl("javascript:window.INTERFACE.processContent(document.getElementsByTagName('body')[0].innerText);");
+            }
+        });
+
+        webView.loadUrl("http://dailypanel.org/article/20150310sae");
+    }
+
+    public void onPause() {
+        if (textspeech_obj != null) {
+            textspeech_obj.stop();
+            textspeech_obj.shutdown();
+        }
+        super.onPause();
+    }
+}
+
+
